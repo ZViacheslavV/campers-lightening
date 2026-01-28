7ca990a1006c63f154606cf998c71c677c0af75f
ZVyachelsavV <vvvv.zkv@gmail.com>
2026-01-28 20:19:58 +0200

    fix container

--- components/ui/Container/Container.tsx
+++ components/ui/Container/Container.tsx
@@ -3,10 +3,10 @@ import clsx from 'clsx';
 
 type ContainerProps = {
   children: React.ReactNode;
-  size?: 'sm' | 'md' | 'lg' | 'xl' | 'fluid';
+  size?: 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | 'fluid';
 };
 
-const Container = ({ children, size = 'lg' }: ContainerProps) => {
+const Container = ({ children, size = 'xxl' }: ContainerProps) => {
   return <div className={clsx(styles.container, styles[size])}>{children}</div>;
 };
 
