c231311aeee98711e3d1b25d1bba42378cf0bd45
ZVyachelsavV <vvvv.zkv@gmail.com>
2026-01-30 14:34:47 +0200

    change order in filters

--- components/Filter/Filter.tsx
+++ components/Filter/Filter.tsx
@@ -1,25 +1,33 @@
 'use client';
 
 import { useState, useCallback } from 'react';
+import clsx from 'clsx';
 
 import { useCampersStore } from '@/lib/store/camperStore';
 import { CamperForm } from '@/types/camper';
 import { CamperFilter, Equipment } from '@/types/filter';
+import { ICONS } from '@/lib/constants';
+import { capitalize } from '@/helpers/capitalize';
 
 import styles from './Filter.module.scss';
 import FilterToggle from './FilterToggle/FilterToggle';
 import RedButton from '../ui/RedButton/RedButton';
-import { capitalize } from '@/helpers/capitalize';
 import { MapIcon } from '../ui/MapIcon/MapIcon';
-import { ICONS } from '@/lib/constants';
-import clsx from 'clsx';
 
-const EQUIPMENT_OPTIONS: readonly Equipment[] = ['AC', 'kitchen', 'TV', 'bathroom'] as const;
+// const EQUIPMENT_OPTIONS: readonly Equipment[] = ['AC', 'kitchen', 'TV', 'bathroom'] as const; //TODO del comments
 
 const VEHICLE_TYPES: readonly { label: string; value: CamperForm }[] = [
   { label: 'Van', value: 'panelTruck' },
   { label: 'Fully Integrated', value: 'fullyIntegrated' },
   { label: 'Alcove', value: 'alcove' },
+] as const;
+
+const VEHICLE_EQUIPMENT_ORDER: (Equipment | 'automatic')[] = [
+  'AC',
+  'automatic',
+  'kitchen',
+  'TV',
+  'bathroom',
 ];
 
 export default function Filters() {
@@ -66,16 +74,11 @@ export default function Filters() {
 
   return (
     <div className={styles.filters}>
+      {/* Location */}
       <div className={styles.field}>
         <label className={styles.label}>Location</label>
         <div className={styles.inputWrapper}>
-          <MapIcon
-            className={clsx(
-              styles.icon,
-              draftFilters.location && draftFilters.location.length > 0 && styles.active
-            )}
-          />
-          <div className={styles.location}></div>
+          <MapIcon className={clsx(styles.icon, draftFilters.location?.length && styles.active)} />
           <input
             type="text"
             placeholder="City"
@@ -87,29 +90,37 @@ export default function Filters() {
 
       <h3 className={styles.sectionTitle}>Filters</h3>
 
+      {/* Vehicle equipment */}
       <h4 className={styles.subTitle}>Vehicle equipment</h4>
-
       <div className={styles.optionsGrid}>
-        <FilterToggle
-          label="Automatic"
-          icon={ICONS.automatic}
-          checked={draftFilters.transmission === 'automatic'}
-          onToggle={toggleTransmission}
-        />
-
-        {EQUIPMENT_OPTIONS.map((item) => (
-          <FilterToggle
-            key={item}
-            label={capitalize(item)}
-            icon={ICONS[item]}
-            checked={draftFilters.equipment?.includes(item) ?? false}
-            onToggle={() => toggleEquipment(item)}
-          />
-        ))}
+        {VEHICLE_EQUIPMENT_ORDER.map((key) => {
+          if (key === 'automatic') {
+            return (
+              <FilterToggle
+                key={key}
+                label="Automatic"
+                icon={ICONS.automatic}
+                checked={draftFilters.transmission === 'automatic'}
+                onToggle={toggleTransmission}
+              />
+            );
+          } else {
+            const equipmentKey = key as Equipment;
+            return (
+              <FilterToggle
+                key={equipmentKey}
+                label={capitalize(equipmentKey)}
+                icon={ICONS[equipmentKey]}
+                checked={draftFilters.equipment?.includes(equipmentKey) ?? false}
+                onToggle={() => toggleEquipment(equipmentKey)}
+              />
+            );
+          }
+        })}
       </div>
 
+      {/* Vehicle type */}
       <h4 className={styles.subTitle}>Vehicle type</h4>
-
       <div className={styles.optionsGrid}>
         {VEHICLE_TYPES.map(({ label, value }) => (
           <FilterToggle
