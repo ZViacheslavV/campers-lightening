57bdf4f3d5ae18a253660b520e72b6992a2e016b
ZVyachelsavV <vvvv.zkv@gmail.com>
2026-01-31 14:53:47 +0200

    adntidataloss crossplatform, camper details page improvements

--- components/CamperPageClient/FormCampervan/FormCampervan.tsx
+++ components/CamperPageClient/FormCampervan/FormCampervan.tsx
@@ -1,3 +1,5 @@
-const FormCampervan = () => <div>Form</div>;
+import styles from './FormCampervan.module.scss';
+
+const FormCampervan = () => <div className={styles.panel}>Form</div>;
 
 export default FormCampervan;
