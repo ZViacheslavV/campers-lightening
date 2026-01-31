5bda945a1a0a96542558a5186bf6e236586bc037
ZVyachelsavV <vvvv.zkv@gmail.com>
2026-01-31 16:00:16 +0200

    finish tabs

--- components/CamperPageClient/CamperDetails/Tabs.tsx
+++ components/CamperPageClient/CamperDetails/Tabs.tsx
@@ -12,6 +12,7 @@ const Tabs = ({ activeTab, onChange }: TabsProps) => {
   const tabsRef = useRef<HTMLDivElement>(null);
   const indicatorRef = useRef<HTMLSpanElement>(null);
 
+  //SLider:
   useEffect(() => {
     const container = tabsRef.current;
     const indicator = indicatorRef.current;
@@ -20,7 +21,7 @@ const Tabs = ({ activeTab, onChange }: TabsProps) => {
     const activeBtn = container.querySelector(`button.${styles.active}`) as HTMLElement;
     if (!activeBtn) return;
 
-    // Плавно задаємо ширину і позицію
+    // Smoothly changing slider position and width
     indicator.style.width = `${activeBtn.offsetWidth}px`;
     indicator.style.transform = `translateX(${activeBtn.offsetLeft}px)`;
   }, [activeTab]);
@@ -49,57 +50,3 @@ const Tabs = ({ activeTab, onChange }: TabsProps) => {
 };
 
 export default Tabs;
-
-// import { useEffect, useRef } from 'react';
-// import styles from './CamperDetails.module.scss';
-
-// export type DetailsTab = 'features' | 'reviews';
-// interface TabsProps {
-//   activeTab: DetailsTab;
-//   onChange: (tab: DetailsTab) => void;
-// }
-
-// //================================================================
-
-// const Tabs = ({ activeTab, onChange }: TabsProps) => {
-//   const tabsRef = useRef<HTMLDivElement>(null);
-
-//   useEffect(() => {
-//     const container = tabsRef.current;
-//     if (!container) return;
-
-//     const activeBtn = container.querySelector(`.${styles.active}`) as HTMLElement;
-//     const indicator = container.querySelector(`.${styles.indicator}`) as HTMLElement;
-
-//     if (!activeBtn || !indicator) return;
-
-//     indicator.style.width = `${activeBtn.offsetWidth}px`;
-//     indicator.style.transform = `translateX(${activeBtn.offsetLeft}px)`;
-//   }, [activeTab]);
-
-//   return (
-//     <div role="tablist" className={styles.tabs}>
-//       <button
-//         role="tab"
-//         aria-selected={activeTab === 'features'}
-//         className={activeTab === 'features' ? styles.active : ''}
-//         onClick={() => onChange('features')}
-//       >
-//         Features
-//       </button>
-
-//       <button
-//         role="tab"
-//         aria-selected={activeTab === 'reviews'}
-//         className={activeTab === 'reviews' ? styles.active : ''}
-//         onClick={() => onChange('reviews')}
-//       >
-//         Reviews
-//       </button>
-
-//       <span className={styles.indicator} />
-//     </div>
-//   );
-// };
-
-// export default Tabs;
